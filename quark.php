2acfa787b7e0828d3ed5f357a042b7f3d65cb200
Andy Miller <rhuk@mac.com>
2017-12-13 21:50:51 -0700

    Moved twig vars to Grav core

--- quark.php
+++ quark.php
@@ -45,34 +45,6 @@ class Quark extends Theme
 
         $twig->twig_vars = array_merge($twig->twig_vars, $form_class_variables);
 
-        $this->grav['twig']->twig()->addFunction(
-            new \Twig_SimpleFunction('bodyclass', [$this, 'getBodyClass'])
-        );
-        $this->grav['twig']->twig()->addFunction(
-            new \Twig_SimpleFunction('themevar', [$this, 'getThemeVar'])
-        );
     }
 
-    public function getThemeVar($var)
-    {
-        return $this->config->get('theme.' . $var, false) ?: '';
-    }
-
-    public function getBodyClass($classes)
-    {
-
-        $header = $this->grav['page']->header();
-        $body_classes = isset($header->body_classes) ? $header->body_classes : '';
-
-        foreach ((array)$classes as $class) {
-            if (!empty($body_classes) && Utils::contains($body_classes, $class)) {
-                continue;
-            } else {
-                $val = $this->config->get('theme.' . $class, false) ? $class : false;
-                $body_classes .= $val ? ' ' . $val : '';
-            }
-        }
-
-        return $body_classes;
-    }
 }
